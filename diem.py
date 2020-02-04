ef1cf868bb89ab1bb99be4a84c112ec63433dab1
Fkat <fkatonline@gmail.com>
2020-02-04 10:56:08 +0700

    sua loi

--- diem.py
+++ diem.py
@@ -6,11 +6,14 @@ from secret import USERNAME, PASSWORD
 class Diem():
     def __init__(self):
         self.driver = webdriver.Firefox()
+        self.driver.get("https://htql.ctump.edu.vn/quanly")
         self.driver.find_element_by_id("quanly").click()
-        self.driver.find_element_by_class_name("selected").find_element_by_id("txt_Login_ten_dang_nhap").send_keys(
-            USERNAME)
-        self.driver.find_element_by_class_name("selected").find_element_by_id("pw_Login_mat_khau").send_keys(PASSWORD)
-        self.driver.find_element_by_class_name("selected").find_element_by_id("pw_Login_mat_khau").submit()
+        self.driver.find_element_by_class_name("selected") \
+            .find_element_by_id("txt_Login_ten_dang_nhap").send_keys(USERNAME)
+        self.driver.find_element_by_class_name("selected") \
+            .find_element_by_id("pw_Login_mat_khau").send_keys(PASSWORD)
+        self.driver.find_element_by_class_name("selected") \
+            .find_element_by_id("pw_Login_mat_khau").submit()
 
     def get_diem(self, mssv, mhp):
         self.driver.get("https://htql.ctump.edu.vn/quanly/diem/xemdiemtoankhoasinhvien")
@@ -21,10 +24,5 @@ class Diem():
         self.driver.find_element_by_id("cmb_s_sr_mhsv").click()
         data_html = self.driver.find_element_by_id("tb_xemdiemtoankhoasinhvien_diemtoankhoa").get_attribute("outerHTML")
         df = pd.read_html(data_html)[0]
-
-        try:
-            diem_list = [float(number) for number in df["Điểm HP tổng hợp"]]
-        except:
-            pass
-        diem_info_dict = {"mssv": mssv, "mhp": mhp, "diem": diem_list}
-        return diem_info_dict
+        diem_list = [number for number in df["Điểm HP tổng hợp"]]
+        return {"mssv": mssv, "mhp": mhp, "diem": diem_list}
